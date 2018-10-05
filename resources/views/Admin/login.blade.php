1eee4ddc973867c8929dd2c2f398bebda2699cdc
matrixinfologics <matrixinfologics@gmail.com>
2018-10-05 10:30:27 +0530

    add: css path on admin login

--- resources/views/Admin/login.blade.php
+++ resources/views/Admin/login.blade.php
@@ -6,7 +6,7 @@
         <title>Fake News Validator - @yield('title')</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
         <script src="{{ asset('js/admin-libs.js') }}"></script>
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
